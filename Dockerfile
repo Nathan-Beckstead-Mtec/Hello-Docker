a6a34964586c20695c9503f15d5046348e758c08
Nathan-Beckstead-Mtec <Nathan.beckstead6282@stu.Mtec.edu>
2023-01-12 11:03:33 -0700

    init

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM node:19
+
+# house-keeping
+LABEL maintainer="Nathan Beckstead"
+LABEL description="A docker hello world node website."
+LABEL cohort="15"
+
+
+# these need to match
+# the ENV variable sets the port for node to host on
+# the EXPOSE exposes it
+ENV PORT=42069
+EXPOSE 42069/tcp
+
+
+WORKDIR /user
+# put files into container
+COPY . .
+
+RUN npm install
+
+CMD ["npm","start"]
+
+# build:
+# sudo docker build -t helloNode:v1.0 .
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const hostname = require("os").hostname();
+
+const app = express()
+const port = 
+    process.env.PORT ??
+    process.env.Port ??
+    process.env.port ??
+    3000;
+
+
+
+
+app.set("views", "./pug");
+app.set("view engine", "pug");
+
+
+app.get("/", (req, res) => {
+    res.render('index', {
+        hostname: hostname
+    });
+})
+
+app.use("/static", express.static("./static"));
+
+
+
+
+
+
+app.listen(port, "0.0.0.0", () => {
+    console.log("listening on port: " + port);
+});
